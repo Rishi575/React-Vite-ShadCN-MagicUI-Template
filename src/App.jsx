72883ffec6e0971645ed275df64417db0bd84931
Rishi575 <rishichidory@gmail.com>
2024-07-23 16:02:13 +0530

    Ui-Frameworks
    #Tailwind UI #ShadCN #Magic Ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from './components/mode-toggle'
+
+const App = () => {
+  return (
+    <>
+    <ModeToggle />  
+    </>
+  )
+}
+
+export default App
